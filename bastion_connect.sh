78c43a3a68553cb28f7c61a3018369bc2ed60bb9
hadeelarshed <hadeel-99@live.com>
2024-05-26 22:02:56 +0300

    final

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -1,1 +1,31 @@
 #!/bin/bash
+
+# Check if KEY_PATH environment variable exists
+if [ -z "${KEY_PATH}" ]; then
+    echo "KEY_PATH env var is expected"
+    exit 5
+fi
+
+# Check if at least one argument is provided
+if [ "$#" -eq 0 ]; then
+    echo "Please provide bastion IP address"
+    exit 5
+fi
+
+# Assign the first argument to the bastion_ip
+bastion_ip=$1
+
+# Check if the second argument exists
+if [ "$#" -gt 1 ]; then
+    private_ip=$2
+
+    # Check if the third argument exists
+    if [ "$#" -gt 2 ]; then
+        command=$3
+        ssh  -o "StrictHostKeyChecking=no" -A -i "${KEY_PATH}" ubuntu@"${bastion_ip}" ssh -i hadeel_key.pem ubuntu@"${private_ip}" "${command}"
+    else
+        ssh -t -o "StrictHostKeyChecking=no" -A -i "${KEY_PATH}" ubuntu@"${bastion_ip}" ssh -t -o "StrictHostKeyChecking=no" -i hadeel_key.pem ubuntu@"${private_ip}"
+    fi
+else
+    ssh -t -o "StrictHostKeyChecking=no" -i "${KEY_PATH}" ubuntu@"${bastion_ip}"
+fi
--- tlsHandshake.sh
+++ tlsHandshake.sh
@@ -1,1 +1,62 @@
 #!/bin/bash
+
+# Check if the server IP address is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <server-ip>"
+    exit 1
+fi
+
+SERVER_IP=$1
+
+# Step 1: Client Hello
+echo "Step 1: Sending Client Hello..."
+CLIENT_HELLO=$(curl -s -X POST -H "Content-Type: application/json" -d '{"version": "1.3", "ciphersSuites": ["TLS_AES_128_GCM_SHA256", "TLS_CHACHA20_POLY1305_SHA256"], "message": "Client Hello"}' http://$SERVER_IP:8080/clienthello)
+if [ $? -ne 0 ]; then
+    echo "Failed to send Client Hello"
+    exit 2
+fi
+
+# Step 2: Server Hello
+echo "Step 2: Received Server Hello..."
+SERVER_HELLO=$(echo "$CLIENT_HELLO" | jq -r '.')
+SESSION_ID=$(echo "$SERVER_HELLO" | jq -r '.sessionID')
+SERVER_CERT=$(echo "$SERVER_HELLO" | jq -r '.serverCert')
+echo "$SERVER_CERT" > server_cert.pem
+
+
+# Step 3: Server Certificate Verification
+echo "Step 3: Verifying Server Certificate..."
+wget https://alonitac.github.io/DevOpsTheHardWay/networking_project/cert-ca-aws.pem
+openssl verify -CAfile cert-ca-aws.pem server_cert.pem > /dev/null
+if [ $? -ne 0 ]; then
+    echo "Server Certificate is invalid."
+    exit 5
+fi
+
+
+# Step 4: Client-Server master-key exchange
+echo "Step 4: Generating and sending master key..."
+MASTER_KEY=$(openssl rand -base64 32)
+ENCRYPTED_MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in <(echo "$MASTER_KEY") -outform DER server_cert.pem | base64 -w 0)
+
+
+#Step 5: Server verification message
+echo "Step 5: Sending master key and sample message..."
+SAMPLE_MESSAGE="Hi server, please encrypt me and send to client!"
+KEY_EXCHANGE=$(curl -s -X POST -H "Content-Type: application/json" -d "{\"sessionID\": \"$SESSION_ID\", \"masterKey\": \"$ENCRYPTED_MASTER_KEY\", \"sampleMessage\": \"$SAMPLE_MESSAGE\"}" http://$SERVER_IP:8080/keyexchange)
+if [ $? -ne 0 ]; then
+    echo "Failed to send master key and sample message"
+    exit 3
+fi
+
+
+# Step 6: Server verification message
+echo "Step 6: Verifying server's symmetric encryption..."
+ENCRYPTED_SAMPLE_MESSAGE=$(echo "$KEY_EXCHANGE" | jq -r '.encryptedSampleMessage')
+DECRYPTED_SAMPLE_MESSAGE=$(echo "$ENCRYPTED_SAMPLE_MESSAGE" | base64 -d | openssl enc -d -aes-256-cbc -pbkdf2 -k "$MASTER_KEY")
+if [ "$DECRYPTED_SAMPLE_MESSAGE" != "$SAMPLE_MESSAGE" ]; then
+    echo "Server symmetric encryption using the exchanged master-key has failed."
+    exit 6
+fi
+
+echo "Client-Server TLS handshake has been completed successfully"
